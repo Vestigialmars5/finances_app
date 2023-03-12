8478c30ad057cfb5df5fec8b9a57ada696547914
Jorge Emilio Peña <jorgemilio2002@gmail.com>
2023-03-11 21:42:54 -0500

    Fixed some typos

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ function NewIncome(props) {
   // set the useState for the incomeAmount
   const [incomeAmount, setIncomeAmount] = useState(0);
 
-  // function to do store the income if the incomeAmount is more than 0
+  // function to store the income if the incomeAmount is more than 0
   function handleStoringIncome() {
     if (incomeAmount > 0) {
+      // lift it up
       props.onStoreIncome(incomeAmount);
+      // reset incomeAmount
       setIncomeAmount(0);
     }
     else {
+      // less than 0 reset incomeAmount
       alert("Can't add add 0 or less than 0 to your income, can you? That would be an expense. Just input a normal positive number o_o")
       setIncomeAmount(0);
     }
@@ -136,17 +139,3 @@ const App = () => {
 
 export default App;
 
-
-/* 1. an app that helps you organice your income expenses and give an analisis on how that money is used.
-2. people that rely on tracking their money in a spreadsheet document. it takes a lot of time and it is not automated.
-3. i want to include a way to add a new expense and be able to categorice it into whatever form of expense it is (medical, groceries,etc). i want it to be able to take in your income and be able to add where that income came from like with the expenses. also adding an optional description for each of those. i want it to display your current money after taking in the input or salary for the month and substracting after each expense. i also want it to be able to display data on those expenses and show where most of the money is used in and other analisis stuff.
-4. i want it to be a website and then i want to develop a mobile app for it so that it can be more portable.
-5. no
-6. im learning about everything so anything goes, im going with react because of its dynamic displaying of elements without reloding each time.
-7. the code provided before is all the stuff being displayed. i created this project using npx create-app... so i have a lot of other files and dependencies. the code before was the app.js in src folder.
-8. no
-9. i dont have a budget or anything, im also the only one working on this. 
-
-this is a project im doing so that i have something to shoe in my portfolio */
-
-/* 54 6f 20 72 65 63 61 70 20 77 68 61 74 20 77 65 20 64 69 73 63 75 73 73 65 64 2c 20 77 65 20 74 61 6c 6b 65 64 20 61 62 6f 75 74 20 74 68 65 20 66 6f 6c 6c 6f 77 69 6e 67 20 70 6f 69 6e 74 73 20 79 6f 75 20 63 6f 75 6c 64 20 61 64 64 20 69 6e 20 74 68 65 20 66 75 74 75 72 65 2e 0a 0a 48 65 72 65 20 61 72 65 20 73 6f 6d 65 20 69 64 65 61 73 20 66 6f 72 20 6d 6f 72 65 20 66 65 61 74 75 72 65 73 20 79 6f 75 20 63 6f 75 6c 64 20 61 64 64 20 69 6e 20 74 68 65 20 66 75 74 75 72 65 3a 0a 31 2e 20 61 6e 20 61 70 70 20 74 68 61 74 20 68 65 6c 70 73 20 79 6f 75 20 6f 72 67 61 6e 69 7a 65 20 79 6f 75 72 20 69 6e 63 6f 6d 65 20 65 78 70 65 6e 73 65 73 20 61 6e 64 20 67 69 76 65 73 20 61 6e 20 61 6e 61 6c 79 73 69 73 20 6f 6e 20 68 6f 77 20 74 68 61 74 20 6d 6f 6e 65 79 20 69 73 20 75 73 65 64 2e 0a 32 2e 20 70 65 6f 70 6c 65 20 74 68 61 74 20 72 65 6c 79 20 6f 6e 20 74 72 61 63 6b 69 6e 67 20 74 68 65 69 72 20 6d 6f 6e 65 79 20 69 6e 20 61 20 73 70 72 65 61 64 73 68 65 65 74 20 64 6f 63 75 6d 65 6e 74 2e 20 69 74 20 74 61 6b 65 */
